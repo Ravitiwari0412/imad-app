81dd00edf9d1d21f725c10d7f904dd7b5c9660b4
RaviTiwari <tiwaririshu19@gmail.com>
2018-03-04 17:12:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article1',function(req,res) {
-    res.send('article1 is requested and article1 will be fetched');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    
     
 });
 app.get('/article2',function(req,res) {
